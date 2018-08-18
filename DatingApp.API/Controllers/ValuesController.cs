11f5552ffadb92cfbe7a6797a39cb39670752625
Allan Beckman <allansbeckman@gmail.com>
2018-08-18 16:49:25 -0700

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    // http://localhost:5000/api/values/5
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext context;
+        public ValuesController(DataContext context)
+        {
+            this.context = context;
+
+        }
+        // GET api/values
+        // IAction result allows us to return http response
+        [HttpGet]
+        public async Task<IActionResult> GetValues() //async method. won't block the thread.
+        {
+            var values = await context.Values.ToListAsync(); // go to database and get values as a list
+
+            return Ok(values); // return values to client as http response
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")] //id represents next part of url.
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await context.Values.FirstOrDefaultAsync(x => x.Id == id);
+
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
